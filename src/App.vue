fe238fadb9f7dc7202d122376483040c2d39a432
Volkov Alexander <volkov.525@gmail.com>
2019-09-09 12:49:54 +0300

    change all to pug and add hide-on-scroll to app bar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
-<template>
-	<v-app>
-		<v-app-bar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Vuetify</span>
-				<span class="font-weight-light">MATERIAL DESIGN</span>
-			</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-btn
-				text
-				href="https://github.com/vuetifyjs/vuetify/releases/latest"
-				target="_blank"
-			>
-				<span class="mr-2">Latest Release</span>
-				<v-icon>mdi-open-in-new</v-icon>
-			</v-btn>
-		</v-app-bar>
-
-		<v-content>
-			<router-view/>
-		</v-content>
-	</v-app>
+<template lang="pug">
+v-app
+	v-app-bar( app hide-on-scroll)
+		v-toolbar-title.headline.text-uppercase
+			span Vuetify
+			span( class="font-weight-light" ) MATERIAL DESIGN
+		v-spacer
+		v-btn( text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" )
+			span( class="mr-2" ) Latest Release
+			v-icon mdi-open-in-new
+	v-content
+		router-view
 </template>
 
 <script>
