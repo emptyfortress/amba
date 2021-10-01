f2d0ab305098e958e6dc769a5d6433c586bd1531
Volkov Alexander <volkov.525@gmail.com>
2021-10-01 15:00:14 +0300

    another try

--- src/components/Files.vue
+++ src/components/Files.vue
@@ -5,37 +5,37 @@
 </template>
 
 <script>
-import doc from "@/assets/img/filetype/doc.svg";
-import pdf from "@/assets/img/filetype/pdf.svg";
-import txt from "@/assets/img/filetype/txt.svg";
-import xls from "@/assets/img/filetype/xls.svg";
+import doc from '@/assets/img/filetype/doc.svg'
+import pdf from '@/assets/img/filetype/pdf.svg'
+import txt from '@/assets/img/filetype/txt.svg'
+import xls from '@/assets/img/filetype/xls.svg'
 
 export default {
-	props: ["data"],
-	data() {
+	props: ['data'],
+	data () {
 		return {
 			doc,
 			pdf,
 			xls,
 			txt
-		};
+		}
 	},
 	computed: {},
 	methods: {
-		ur(e) {
+		ur (e) {
 			switch (e) {
-				case "doc":
-					return this.doc;
-				case "pdf":
-					return this.pdf;
-				case "txt":
-					return this.txt;
-				case "xls":
-					return this.xls;
+			case 'doc':
+				return this.doc
+			case 'pdf':
+				return this.pdf
+			case 'txt':
+				return this.txt
+			case 'xls':
+				return this.xls
 			}
 		}
 	}
-};
+}
 </script>
 
 <style scoped lang="scss">
--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	// publicPath: process.env.NODE_ENV === 'production'
 	// ? '/amba/'
 	// : '/',
-	chainWebpack: config => {
-		config.module.rule('eslint').use('eslint-loader').options({
-			fix: true
-		})
-	}
+	// chainWebpack: config => {
+	// 	config.module.rule('eslint').use('eslint-loader').options({
+	// 		fix: true
+	// 	})
+	// }
 }
